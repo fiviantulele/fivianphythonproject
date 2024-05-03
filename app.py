261b01722fc9c2041cceaab50bbccd478c9629be
fiviantulele <168805065+fiviantulele@users.noreply.github.com>
2024-05-03 09:56:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.write('hello world, my first streamlit program')
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def main():
+    st.title("Celsius to Fahrenheit Converter")
+    
+    celsius_temp = st.number_input("Enter temperature in Celsius", value=0.0)
+    
+    fahrenheit_temp = celsius_to_fahrenheit(celsius_temp)
+    
+    st.write(f"{celsius_temp} Celsius is equal to {fahrenheit_temp} Fahrenheit.")
+
+if __name__ == "__main__":
+    main()
+
